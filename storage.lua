ca88f19658eaedd836885560c74708a0882039c5
lgtyqz <kevinlyang01@gmail.com>
2017-03-14 14:47:34 -0700

    Moar Fonts

--- storage.lua
+++ storage.lua
@@ -1,5 +1,6 @@
 t = {
 	headerFont = native.newFont("./Fonts/OSWALD/Oswald-Medium.ttf"),
-	bodyFont = native.newFont("./Fonts/LATO/Lato-Light.ttf")
+	bodyFont = native.newFont("./Fonts/LATO/Lato-Light.ttf"),
+	buttonFont = native.newFont("./Fonts/LATO/Lato-Regular.ttf")
 }
 return t

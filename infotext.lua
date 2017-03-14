2f57c87bc1c1e7d3db599385ce58d4de31e60707
lgtyqz <kevinlyang01@gmail.com>
2017-03-14 16:00:02 -0700

    INFO TEXT
    (reading not implemented yet)

---
+++ infotext.lua
@@ -0,0 +1,49 @@
+local storage = require("storage")
+local t = {
+	{
+		title: "What is bullying?",
+		subtitle: nil,
+		text: [[
+		Bullying is defined as:
+			"An intentional behavior that hurts, harms, or humiliates a student, either physically or emotionally, and can happen while at school, in the community, or online. Those bullying often have more social or physical "power," while those targeted have difficulty stopping the behavior. The behavior is typically repeated, though it can be a one-time incident.
+Students often describe bullying as when "someone makes you feel less about who you are as a person."¹
+		]],
+		image: nil
+	},
+	{
+		title: "The Types of Bullying",
+		subtitle: "Physical Bullying"
+		text: [[
+		Physical bullying is probably the most obvious form of bullying.
+		Physical bullying includes:
+		◆ Punching
+		◆ Kicking
+		◆ Beating someone up
+		◆ Damaging property
+		It can cause both short-term and long-term damage.²
+		]],
+		image: nil
+	},
+		title: "The Types of Bullying",
+		subtitle: "Verbal Bullying"
+		text: [[
+		Verbal bullying directly verbally harms the victim.
+		Verbal bullying includes:
+		◆ Name-calling/Insults
+		◆ Intimidation
+		◆ Homophobic or racist remarks
+		◆ Verbal Abuse
+		Verbal bullying may start off mostly harmless, but it can severely demoralize the victim and possibly drive them to suicide.²
+		]],
+		image: nil
+	},
+}
+--[[
+Sources:
+1 - http://www.pacer.org/bullying/resources/questions-answered/how-is-bullying-defined.asp
+2 - https://www.ncab.org.au/bullying-advice/bullying-for-parents/types-of-bullying/
+3 - 
+4 - 
+5 - 
+]]--
+return t

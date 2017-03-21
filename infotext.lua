838b75a3b543350fa7e5a21f130eb98e64a93471
lgtyqz <kevinlyang01@gmail.com>
2017-03-21 15:32:45 -0700

    SYNTAX ERRORS FIXED

--- infotext.lua
+++ infotext.lua
@@ -1,19 +1,19 @@
 local storage = require("storage")
 local t = {
 	{
-		title: "What is bullying?",
-		subtitle: nil,
-		text: [[
+		title="What is bullying?",
+		subtitle=nil,
+		text= [[
 		Bullying is defined as:
 			"An intentional behavior that hurts, harms, or humiliates a student, either physically or emotionally, and can happen while at school, in the community, or online. Those bullying often have more social or physical "power," while those targeted have difficulty stopping the behavior. The behavior is typically repeated, though it can be a one-time incident.
 Students often describe bullying as when "someone makes you feel less about who you are as a person."¹
 		]],
-		image: nil
+		image= nil
 	},
 	{
-		title: "The Types of Bullying",
-		subtitle: "Physical Bullying"
-		text: [[
+		title= "The Types of Bullying",
+		subtitle= "Physical Bullying",
+		text= [[
 		Physical bullying is probably the most obvious form of bullying.
 		Physical bullying includes:
 		◆ Punching
@@ -22,11 +22,12 @@ Students often describe bullying as when "someone makes you feel less about who 
 		◆ Damaging property
 		It can cause both short-term and long-term damage.²
 		]],
-		image: nil
+		image= nil
 	},
-		title: "The Types of Bullying",
-		subtitle: "Verbal Bullying"
-		text: [[
+	{
+		title= "The Types of Bullying",
+		subtitle= "Verbal Bullying",
+		text= [[
 		Verbal bullying directly verbally harms the victim.
 		Verbal bullying includes:
 		◆ Name-calling/Insults
@@ -35,9 +36,11 @@ Students often describe bullying as when "someone makes you feel less about who 
 		◆ Verbal Abuse
 		Verbal bullying may start off mostly harmless, but it can severely demoralize the victim and possibly drive them to suicide.²
 		]],
-		image: nil
-	},
+		image= nil
+	}
 }
+
+
 --[[
 Sources:
 1 - http://www.pacer.org/bullying/resources/questions-answered/how-is-bullying-defined.asp
